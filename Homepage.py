7b81f1aa29206fbd82afe3c98208aba830331687
Wan Izzati <wannurizzatiwanabdazizktb@gmail.com>
2025-10-27 21:19:53 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Add a banner image at the top
-banner_image = 'https://github.com/wannurizzatiwanabdazizktb-arch/SV-1/blob/057cb9d2e12f38cbfe1eda6a802a41fcc7ba2e0d/Online%20Learning.jpg' 
+banner_image = 'https://github.com/wannurizzatiwanabdazizktb-arch/SV-1/blob/main/Online%20Learning.jpeg' 
 st.image(banner_image, use_container_width=True)
 
 # Add the main introduction paragraph
