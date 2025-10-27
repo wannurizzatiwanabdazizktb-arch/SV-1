c2ade4b6e689a6463a10361ba583ffecbb59ef00
Wan Izzati <wannurizzatiwanabdazizktb@gmail.com>
2025-10-27 21:08:02 +0800

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Student Survey"
+)
+
+visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
+
+home = st.Page('Homepage.py', title='Homepage', default=True, icon=":material/home:")
+
+pg = st.navigation(
+        {
+            "Menu": [home, visualise]
+        }
+    )
+
+pg.run()
