94a0940d21ffc048a2acefac4731555a4f3b3ade
Wan Izzati <wannurizzatiwanabdazizktb@gmail.com>
2025-10-30 01:07:12 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -14,20 +14,8 @@ st.write(
 banner_image = 'https://raw.githubusercontent.com/wannurizzatiwanabdazizktb-arch/SV-1/main/OnlineLearning.jpg' 
 st.image(banner_image, use_container_width=True)
 
-banner_image = 'https://raw.githubusercontent.com/fakhitah3/FHPK-TVET/main/3u1i_2.jpeg' 
-st.image(banner_image, use_container_width=True)
-
 # Add the extended explanation
 st.write(
-    """
-    The aim of scientific visualization is not merely to present data attractively, but to **enhance comprehension and decision-making** through visual analytics. 
-    Applications span across disciplines such as **climate science**, **medicine**, **engineering**, **data science**, and **environmental studies**.
-
-    In this course or module, students will learn to:
-    - Select relevant datasets for analysis and visualization.
-    - Apply various visualization techniques such as graphs, maps, and 3D models.
-    - Interpret visual outputs to support scientific conclusions and policy recommendations.
-    
-    By the end of this exercise, students should be able to produce **informative, accurate, and interactive visualizations** that effectively communicate scientific findings to both expert and non-expert audiences.
+    """This dashboard presents insights from collected data on students’ experiences in online education. Explore the three sections below to learn about performance impact, satisfaction levels, and challenges faced.
     """
 )
