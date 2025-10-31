4deca601b913754a3938100b1b2c9addcf58eaad
Wan Izzati <wannurizzatiwanabdazizktb@gmail.com>
2025-11-01 07:12:19 +0800

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -4,13 +4,15 @@ st.set_page_config(
     page_title="Online Learning Survey"
 )
 
-visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
+visualise = st.Page('StudentSatisfaction.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
+
+visualise = st.Page('PerformanceImpact.py', title='Pencapaian Akademik Pelajar', icon=":material/result:")
 
 home = st.Page('Homepage.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
         {
-            "Menu": [home, visualise]
+            "Menu": [home, visualise, visualise]
         }
     )
 
